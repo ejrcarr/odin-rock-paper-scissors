6a10b773deff9c1eebb1819131f7fe99fa725917
Evan Carr <ejrcarr@gmail.com>
2023-01-15 20:07:00 -0500

    Added onClick effects to button UI

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
 
+const playerScoreSpan = document.getElementById('player-score');
+const computerScoreSpan = document.getElementById('computer-score');
+
+const commentaryPara = document.querySelector('.commentary');
+
+const playerRock = document.getElementById('player-rock');
+const playerPaper = document.getElementById('player-paper');
+const playerScissors = document.getElementById('player-scissors');
+
+const computerRock = document.getElementById('computer-rock');
+const computerPaper = document.getElementById('computer-paper');
+const computerScissors = document.getElementById('computer-scissors');
+
 function getComputerChoice() {
 	let choices = ['Rock', 'Paper', 'Scissors'];
 	return choices[Math.floor(Math.random() * 3)];
@@ -11,63 +24,117 @@ function playRound(playerSelection, computerSelection) {
 	if (!playerSelection) {
 		return null;
 	}
+	let playerScore = parseInt(playerScoreSpan.textContent);
+	let computerScore = parseInt(computerScoreSpan.textContent);
 	let lowerPlayerText = playerSelection.toLowerCase().trim();
 	let lowerComputerText = computerSelection.toLowerCase().trim();
+	updateRoundIcons(lowerPlayerText, lowerComputerText);
 	if (lowerPlayerText === lowerComputerText) {
-		console.log('Tie!');
+		commentaryPara.textContent = 'Tie!';
 		return null;
 	} else if (lowerPlayerText == 'paper' && lowerComputerText == 'rock') {
-		console.log(`You win! Paper beats rock!`);
+		commentaryPara.textContent = `You win this round! Paper beats rock!`;
+		updateScores(playerScore + 1, computerScore);
 		return true;
 	} else if (lowerPlayerText == 'scissors' && lowerComputerText == 'paper') {
-		console.log(`You win! Scissors beats paper!`);
+		commentaryPara.textContent = `You win this round! Scissors beats paper!`;
+		updateScores(playerScore + 1, computerScore);
 		return true;
 	} else if (lowerPlayerText == 'rock' && lowerComputerText == 'scissors') {
-		console.log(`You win! Rock beats scissors!`);
+		commentaryPara.textContent = `You win this round! Rock beats scissors!`;
+		updateScores(playerScore + 1, computerScore);
 		return true;
 	} else {
 		let computerChoice =
 			computerSelection.charAt(0).toUpperCase() +
 			computerSelection.substr(1).toLowerCase();
-		console.log(`You lose! ${computerChoice} beats ${lowerPlayerText}!`);
+		commentaryPara.textContent = `You lose this round! ${computerChoice} beats ${lowerPlayerText}!`;
+		updateScores(playerScore, computerScore + 1);
 		return false;
 	}
 }
 
-function game() {
-	let playerScore = 0;
-	let computerScore = 0;
-	while (playerScore < 5 && computerScore < 5) {
-		let playerSelection = prompt('What would you like to play?');
-		let computerSelection = getComputerChoice();
-		let result = playRound(playerSelection, computerSelection);
-		if (result) {
-			playerScore++;
-		} else if (result !== null) {
-			computerScore++;
+function handleButtonClicks() {
+	rockButton.addEventListener('click', assignRockButton);
+	paperButton.addEventListener('click', assignPaperButton);
+	scissorsButton.addEventListener('click', assignScissorsButton);
+}
+
+function assignRockButton() {
+	let result = playRound('rock', getComputerChoice());
+	rockButton.classList.add('clicked');
+	setTimeout(() => {
+		rockButton.classList.remove('clicked');
+	}, 200);
+}
+
+function assignPaperButton() {
+	let result = playRound('paper', getComputerChoice());
+	paperButton.classList.add('clicked');
+	setTimeout(() => {
+		paperButton.classList.remove('clicked');
+	}, 200);
+}
+
+function assignScissorsButton() {
+	let result = playRound('scissors', getComputerChoice());
+	scissorsButton.classList.add('clicked');
+	setTimeout(() => {
+		scissorsButton.classList.remove('clicked');
+	}, 200);
+}
+
+function updateScores(playerPoints, computerPoints) {
+	playerScoreSpan.textContent = playerPoints;
+	computerScoreSpan.textContent = computerPoints;
+
+	if (playerPoints >= 5 || computerPoints >= 5) {
+		if (playerPoints > computerPoints) {
+			commentaryPara.textContent = 'Congratulations! You win!';
+		} else if (playerPoints < computerPoints) {
+			commentaryPara.textContent = 'You lose!';
+		} else {
+			commentaryPara.textContent = 'It was a tie! Try again!';
 		}
-	}
-	if (playerScore > computerScore) {
-		console.log('Congratulations! You win!');
-	} else if (playerScore < computerScore) {
-		console.log('You lose!');
-	} else {
-		console.log('It was a tie! Try again!');
+		quitGame();
+		return;
 	}
 }
 
-function handleButtonClicks() {
-	rockButton.addEventListener('click', () => {
-		console.log('rock');
+function updateRoundIcons(playerSelection, computerSelection) {
+	let playerChoice = 'player-' + playerSelection;
+	let computerChoice = 'computer-' + computerSelection;
+	let playerIcons = document.querySelectorAll(
+		'.current-round .player-selection > i'
+	);
+	let computerIcons = document.querySelectorAll(
+		'.current-round .computer-selection > i'
+	);
+
+	playerIcons.forEach((icon) => {
+		console.log();
+		if (icon.id === playerChoice && !icon.classList.contains('active')) {
+			console.log('big here');
+			icon.classList.add('active');
+		} else if (icon.id !== playerChoice) {
+			console.log('here');
+			icon.classList.remove('active');
+		}
 	});
 
-	paperButton.addEventListener('click', () => {
-		console.log('paper');
+	computerIcons.forEach((icon) => {
+		if (icon.id === computerChoice && !icon.classList.contains('active')) {
+			icon.classList.add('active');
+		} else if (icon.id !== computerChoice) {
+			icon.classList.remove('active');
+		}
 	});
+}
 
-	scissorsButton.addEventListener('click', () => {
-		console.log('scissors');
-	});
+function quitGame() {
+	rockButton.removeEventListener('click', assignRockButton);
+	paperButton.removeEventListener('click', assignPaperButton);
+	scissorsButton.removeEventListener('click', assignScissorsButton);
 }
 
 handleButtonClicks();
